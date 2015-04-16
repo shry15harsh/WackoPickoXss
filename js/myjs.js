2157153514896263f88b3e4cbe53fe09675fda68
Harsh <shry.harsh@gmail.com>
2015-04-17 00:01:53 +0530

    login and register page check

--- js/myjs.js
+++ js/myjs.js
@@ -17,3 +17,37 @@ function commentSubmit(){
 	}
 	return true;
 }
+
+function loginOnSubmit(){
+	var query = $('#username').val();
+	var filter_query = query;
+	if(query!=filterXSS(filter_query)){
+		alert('XSS performed');
+//		$('#username').val(filterXSS(filter_query));
+	}
+	return true;
+}
+
+function registerCheck(){
+	var query = $('#username').val();
+	var filter_query = query;
+	if(query!=filterXSS(filter_query)){
+		alert('XSS performed in Username Field');
+		return false;
+	}
+
+	query = $('#firstname').val();
+	filter_query = query;
+	if(query!=filterXSS(filter_query)){
+		alert('XSS performed in Firstname Field');
+		return false;
+	}
+
+	query = $('#lastname').val();
+	filter_query = query;
+	if(query!=filterXSS(filter_query)){
+		alert('XSS performed in Lastname Field');
+		return false;
+	}
+	return true;
+}
